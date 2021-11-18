c96676f0be967ec0a6a54b6c76f5a21f0485f5e2
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-18 14:26:01 -0300

    Criar classe Produto

--- src/main/java/br/com/zup/leadCollector/produto/Produto.java
+++ src/main/java/br/com/zup/leadCollector/produto/Produto.java
@@ -1,2 +1,19 @@
-package br.com.zup.leadCollector.produto;public class Produto {
+package br.com.zup.leadCollector.produto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true)
+    private String nome;
 }
---
+++ src/main/java/br/com/zup/leadCollector/produto/ProdutoRepository.java
@@ -0,0 +1,2 @@
+package br.com.zup.leadCollector.produto;public interface ProdutoRepository {
+}
